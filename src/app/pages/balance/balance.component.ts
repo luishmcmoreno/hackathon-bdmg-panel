ab6c1fc84c1630f82ec859c1003e38abc7a1e379
mduraes1994 <mduraes1994@gmail.com>
2017-11-02 18:14:21 -0200

    Detalhes do projeto

--- src/app/pages/balance/balance.component.ts
+++ src/app/pages/balance/balance.component.ts
@@ -8,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class BalanceComponent implements OnInit {
 
   public moneyChartLabels: string[] = ['Investimento', 'Retorno'];
-  public moneyPieChartData: number[] = [450, 500];
+  public moneyPieChartData: number[] = [450000, 550000];
 
   public projectsLabel: string[] = ['Aprovados', 'À analisar'];
   public projectPieChartData: number[] = [127, 21];
+  public moneyColors: Array<any> = [{
+      backgroundColor: ['rgba(47, 70, 181, 1)', 'rgba(75, 175, 80, 1)']}
+  ];
+
+  public projectColors: Array<any> = [{
+      backgroundColor: ['rgba(75, 175, 80, 1)', 'rgba(236, 214, 213, 0.72)', ]}
+  ];
 
   public pieType = 'pie';
 
